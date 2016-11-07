dbb6fdeae236bc5d9cc1e94feb181c50250bcca7
Trevor Morse <mors6189@vandals.uidaho.edu>
2016-11-07 14:21:31 -0800

    moved rgb struct to separate header file

---
+++ rgb.h
@@ -0,0 +1,16 @@
+/**
+* CS 383
+* Group #4
+* Created: 11/07/2016
+*/
+
+/**
+* Structure for storing sets of rgb color values.
+*/
+struct RGB
+{
+	int red;
+	int green;
+	int blue;
+};
+typedef struct RGB rgb;
