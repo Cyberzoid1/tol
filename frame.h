b084a335a29f31c5ea8adf11a07140db830bd95c
fetr0509 <Alpha@Peters-MacBook-Pro.local>
2016-11-07 17:03:49 -0800

    Updated Frame Interface
    Added new methods to the frame class.

--- frame.h
+++ frame.h
@@ -1,26 +1,33 @@
-//
-//  frame.h
-//  
-//
 //  Created by Peter Fetros on 11/7/16.
-//
-//
+
 
 #ifndef FRAME_H
 #define FRAME_H
+
+#include "Cell.h"
+#include <string>
+#include <vector>
 
 class Frame {
 public:
     Frame();
     Frame(int width, int height);
+    Frame(Frame* newFrame);
     ~Frame();
     
-    int getCellNumber();
-    int getStartTime;
+    int getFrameNumber();
+    void setFrameNumber(int number);
+    int getStartTime();
+    void setStartTime(int time);
+    
+    void setCellColor(int x, int y, int red, int green, int blue);
+    void setCellColor(int x, int y, rgb color);
+    rgb getCellColor(int x, int y);
+
     std::string toString();
     
 private:
-    Cell cells[][];
+    std::vector<std::vector<Cell>> cells;
     int frameNumber;
     int startTime;
 };
