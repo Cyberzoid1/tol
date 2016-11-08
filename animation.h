19d1b0f31680d57d37a8fa1d3387ea8c31d4a030
Trevor Morse <Trevor Morse>
2016-11-07 20:49:09 -0800

    updated interface for Animation class - moved rgb struct to separate file - modified signature of addFrame - modified/added two methods for removing frames

--- animation.h
+++ animation.h
@@ -1,71 +1,62 @@
 /**
- * CS 383
- * Group #4
- * Created: 11/05/2016
- *
- * This is the header file for the Animation class. This class is utilized to store
- * the sequence of frames in the Tower of Light's animation as well as the header info
- * for the resulting .tan2 file.
- */
+* CS 383
+* Group #4
+* Created: 11/05/2016
+*
+* This is the header file for the Animation class. This class is utilized to store
+* the sequence of frames in the Tower of Light's animation as well as the header info
+* for the resulting .tan2 file.
+*/
 
 #ifndef ANIMATION_H
 #define ANIMATION_H
 
 #include <list>
 #include <string>
-#include <tuple>
 #include "frame.h"
+#include "rgb.h"
 
-//number of recently used colors that are stored by the ToL editor
+///number of recently used colors that are stored by the ToL editor
 const int NUMCOLORS = 16;
 
-/*
- * Structure for storing sets of rgb color values.
- */
-struct RGB
-{
-	int red;
-	int green;
-	int blue;
-};
-typedef struct RGB rgb;
-/*
- * Class for storing a list of frames and other information related to
- * the overall animation.
- */
-class Animation{
-   private:
-      std::list<Frame> frames;
-	  double version;
-	  std::string filename;
-	  rgb lastColor;
-	  rgb recentColors[NUMCOLORS];
-	  int numFrames;
-	  int height;
-	  int width;
+/**
+* Class for storing a list of frames and other information related to
+* the overall animation.
+*/
+class Animation {
+private:
+	std::list<Frame> frames;
+	double version;
+	std::string filename;
+	rgb lastColor;
+	rgb recentColors[NUMCOLORS];
+	int numFrames;
+	int height;
+	int width;
 
-   public:
-	   Animation();
-	   ~Animation();
-	   void setFrames(std::list<Frame> frms);
-	   std::list<Frame> getFrames();
-	   void setVersion(double ver);
-	   double getVersion();
-	   void setFilename(std::string fname);
-	   std::string getFilename();
-	   void setLastColor(int red, int green, int blue);
-	   rgb getLastColor();
-	   void setRecentColors(rgb *recColors);
-	   rgb* getRecentColors();
-	   int getNumFrames();
-	   void setHeight(int h);
-	   int getHeight();
-	   void setWidth(int w);
-	   int getWidth();
+public:
+	Animation();
+	~Animation();
+	void setFrames(std::list<Frame> frms);
+	std::list<Frame> getFrames();
+	void setVersion(double ver);
+	double getVersion();
+	void setFilename(std::string fname);
+	std::string getFilename();
+	void setLastColor(int red, int green, int blue);
+	rgb getLastColor();
+	void setRecentColors(rgb *recColors);
+	rgb* getRecentColors();
+	int getNumFrames();
+	void setHeight(int h);
+	int getHeight();
+	void setWidth(int w);
+	int getWidth();
 
-	   void addFrame(Frame frame);
-	   void duplicateFrame(Frame frame);
-	   void removeFrame(Frame frame);
-	   std::string toString();
+	void addFrame(int w, int h, int pos);
+	void duplicateFrame(Frame frame);
+	void removeFrame(int pos);
+	void removeFrames(int first, int last);
+	std::string toString();
 };
 #endif
