56e6ef719f7fc7c7660d5601820ede0aa948d2b2
Trevor Morse <mors6189@vandals.uidaho.edu>
2016-11-16 21:31:00 -0800

    added new function stubs

--- tol_editor/input.h
+++ tol_editor/input.h
@@ -20,6 +20,8 @@ void handleLastColor(std::string data, Animation *animation);
 void handleRecentColors(std::string data, Animation *animation);
 void handleConfigInfo(std::string data, Animation *animation);
 void readHeader(std::ifstream &tanfile, Animation *animation);
+int getActualWidth(std::ifstream &tanfile);
+int getActualHeight(std::ifstream &tanfile);
 void handleRowOfCells(int rowNum, std::string data, Frame *frame, int width);
 void readFrames(std::ifstream &tanfile, Animation *animation);
 Animation readInAnimation(const char *filename);
