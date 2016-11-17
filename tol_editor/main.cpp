7bc9dd06abd04cf0355f337853a01e8d747a2b12
adonaygy <berh9877@vandals.uidaho.edu>
2016-11-16 16:57:07 -0800

    Code does not compile yet, still trying to figure out the instantiation in main.
    Includes basic layout of GUI and Class instantiation of MainWindow
    (Parent) and EditorWindow (Child).

--- tol_editor/main.cpp
+++ tol_editor/main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
+#include "editorwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    MainWindow mainwindow;
+//    EditorWindow * editorwindow = new EditorWindow(&mainwindow);
+    mainwindow.show();
+//    editorwindow -> show();
 
     return a.exec();
 }
