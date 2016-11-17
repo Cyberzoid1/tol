e50a40402c6f05a20c0ef9e8653a0cf312e0cbac
mholman5721 <mholman@alumni.uidaho.edu>
2016-11-16 21:07:23 -0800

    this is a slightly updated version of my branch - please ignore

--- tol_editor/mainwindow.h
+++ tol_editor/mainwindow.h
@@ -15,6 +15,39 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_redBox_valueChanged(int arg1);
+
+    void on_greenBox_valueChanged(int arg1);
+
+    void on_blueBox_valueChanged(int arg1);
+
+    void on_redSlider_valueChanged(int value);
+
+    void on_greenSlider_valueChanged(int value);
+
+    void on_blueSlider_valueChanged(int value);
+
+    void on_colorHex_returnPressed();
+
+    void on_addFrameStart_valueChanged(int arg1);
+
+    void on_addFrameAmount_valueChanged(int arg1);
+
+    void on_addFrameConfirm_clicked();
+
+    void on_removeFrameFirst_valueChanged(int arg1);
+
+    void on_removeFrameLast_valueChanged(int arg1);
+
+    void on_removeFrameConfirm_clicked();
+
+    void on_copyFrameSourceIndex_valueChanged(int arg1);
+
+    void on_copyFrameDestinationIndex_valueChanged(int arg1);
+
+    void on_copyFrameConfirmation_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
